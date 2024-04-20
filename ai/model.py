738f7dcd5a60487cc8d9cacecf0d691ec94376eb
Mzyhh <xiiad@mail.ru>
2024-04-20 18:59:45 +0300

    extract_station working better

--- ai/model.py
+++ ai/model.py
@@ -6,13 +6,13 @@ connection = sqlite3.connect('../data/databases/train_database.sqlite')
 cursor = connection.cursor()
 stations = [''.join(x).split('(')[0].lower() for x in cursor.execute('SELECT NAME FROM STATION').fetchall()]
 def extract_station(text):
-    text = text.lower().split()
+    text = ''.join(text.split()).lower()
     min_distance = float("inf")
     min_distance_station = None
     for station in stations:
         tmp = ''.join(station.split())
-        for i in range(len(text) - len(station.split()) + 1):
-            w = ''.join(text[i:i + len(station.split())])
+        for i in range(len(text) - len(tmp) + 1):
+            w = ''.join(text[i:i + len(tmp)])
             cur_distance = distance(w, tmp)
             if cur_distance < min_distance:
                 min_distance = cur_distance
@@ -24,11 +24,14 @@ morph_vocab = MorphVocab()
 dates_extractor = DatesExtractor(morph_vocab)
 def extract_date(text):
     date = list(dates_extractor(text))
+    if date == []:
+        return (None, None, None)
     fact = date[0].__getattribute__("fact")
     return (fact.__getattribute__("day"), fact.__getattribute__("month"), fact.__getattribute__("year"))
 
 
 if __name__ == "__main__":
-    text = '21 апреля я провел целый день, исследуя красоту мозаик на станции майяковская.'
+    text = 'Оказавшись на "Площади Революции", ощутила себя участницей большой театральной постановки'
     print(extract_station(text))
     print(extract_date(text))
+    # print(stations)
