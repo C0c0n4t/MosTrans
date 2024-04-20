986b9a2cfde56cae118afdcdb38829d46beb8cff
Mzyhh <xiiad@mail.ru>
2024-04-20 18:19:36 +0300

    Working ejji

--- ai/model.py
+++ ai/model.py
@@ -4,18 +4,22 @@ import sqlite3
 
 connection = sqlite3.connect('../data/databases/train_database.sqlite')
 cursor = connection.cursor()
-stations = [''.join(x).split()[0].split('-')[0].lower() for x in cursor.execute('SELECT NAME FROM STATION').fetchall()]
-def extract(text):
-    text = text.lower()
+stations = [''.join(x).split('(')[0].lower() for x in cursor.execute('SELECT NAME FROM STATION').fetchall()]
+def extract_station(text):
+    text = text.lower().split()
     min_distance = float("inf")
     min_distance_station = None
     for station in stations:
-        cur_distance = distance(text, station)
-        if cur_distance < min_distance:
-            min_distance = cur_distance
-            min_distance_station = station
+        tmp = ''.join(station.split())
+        for i in range(len(text) - len(station.split()) + 1):
+            w = ''.join(text[i:i + len(station.split())])
+            cur_distance = distance(w, tmp)
+            if cur_distance < min_distance:
+                min_distance = cur_distance
+                min_distance_station = station
+        
     return min_distance_station
 
 if __name__ == "__main__":
-    text = 'В прошлую пятницу я был поражен количеством людей, которые едут на работу через станцию библиотека имени ленина.'
-    print(extract(text))
+    text = 'Вчера я провел целый день, исследуя красоту мозаик на станции майяковская.'
+    print(extract_station(text))
